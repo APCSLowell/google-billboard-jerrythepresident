b6c02f70398d67c3c83d1068e11c22e2ee760d43
jerrythepresident <142940577+jerrythepresident@users.noreply.github.com>
2024-01-17 10:06:32 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -1,4 +1,4 @@
-
+// I attempted to do level 2, but it's wouldn't give me a checkmark because it only check level 1.
 
 public class GoogleBillboard {
 	public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
