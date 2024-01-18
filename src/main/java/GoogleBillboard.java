aa5b80af6781c1b2e5a0011945584ed3e803a700
jerrythepresident <142940577+jerrythepresident@users.noreply.github.com>
2024-01-17 21:37:28 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -13,7 +13,7 @@ while ((!primeFound) && startIndex + consecutiveDigits <= e.length()) {
         double parsedDouble = Double.parseDouble(substring);
         
     if (isPrime(parsedDouble)) {
-                  return substring;
+                  System.out.println(substring);
                   primeFound = true;
               } 
 
