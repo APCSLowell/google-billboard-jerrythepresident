f9683246b291430fe4b4e108c4c3db6855585d83
jerrythepresident <142940577+jerrythepresident@users.noreply.github.com>
2024-01-17 21:35:06 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -2,9 +2,9 @@
 // the link to my code is https://github.com/jerrythepresident/GoogleBillboard/blob/master/GoogleBillboard.pde, it works perfectly on processing.
 
 public class GoogleBillboard {
+	public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
 	public static void main(final String[] args) {
-public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
-	int consecutiveDigits = 10;
+  int consecutiveDigits = 10;
   int startIndex = 1;
   boolean primeFound = false;
 
@@ -21,7 +21,6 @@ while ((!primeFound) && startIndex + consecutiveDigits <= e.length()) {
        }
 	
 	}
-	
 public static boolean isPrime(double dNum){  
   if (dNum<2){
     return false;
@@ -32,4 +31,5 @@ public static boolean isPrime(double dNum){
   }
   }
   return true;
+}
 }
